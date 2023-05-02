f005fc128da410899c06e73de4c798d6a4b82f25
rsnirob <niroborin111@gmail.com>
2023-05-02 10:25:54 +0600

    marquee install and website title update

--- src/pages/SharedPages/Header/Header.jsx
+++ src/pages/SharedPages/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
-import { Container } from 'react-bootstrap';
+import { Badge, Button, Container } from 'react-bootstrap';
 import logo from '../../../assets/logo.png'
+import Marquee from 'react-fast-marquee';
 
 const Header = () => {
     return (
@@ -12,7 +13,16 @@ const Header = () => {
                     <img className='w-25' src={logo} alt="" />
                     <p className='text-secondary'><small>Welcome to our website</small></p>
                 </div>
-
+                <div>
+                    {/* <Marquee className='text-danger' speed={100}>
+                        We've rounded up our highest-rated recipes of all time! They're our most loved, commented and rated recipes from our big Taste of Home community, all in one place. Check out these new recipes that you have to try this month
+                    </Marquee> */}
+                    <Marquee speed={100}>
+                        <span>Looking for new recipes to try? Check out our latest additions:</span>
+                        <span><Badge variant="success">Grilled salmon with mango salsa</Badge> | <Badge variant="info">Creamy mushroom risotto</Badge> | <Badge variant="warning">Spicy chicken tacos</Badge></span>
+                        <span>Don't forget to bookmark your favorites!</span>
+                    </Marquee>
+                </div>
             </Container>
 
             <NavBar />
