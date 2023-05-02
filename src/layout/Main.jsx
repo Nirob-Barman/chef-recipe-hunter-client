6bca71cc100c6b37200669fc7f6ff55b5eab5d3a
rsnirob <niroborin111@gmail.com>
2023-05-02 09:05:41 +0600

    react router setup and adding router

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../pages/SharedPages/Header/Header';
+import Footer from '../pages/SharedPages/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
